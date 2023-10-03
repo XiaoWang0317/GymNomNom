7ee7a79f8325f09268681028b032f3d27655a95a
XiaoWang <xxw851@case.edu>
2023-10-02 23:01:04 -0400

    Initial commit

---
+++ src/main/java/com/gymnomnom/gymnomnom/GymnomnomApplication.java
@@ -0,0 +1,13 @@
+package com.gymnomnom.gymnomnom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymNomNomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymNomNomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gymnomnom/gymnomnom/GymNomNomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gymnomnom.gymnomnom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymNomNomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
