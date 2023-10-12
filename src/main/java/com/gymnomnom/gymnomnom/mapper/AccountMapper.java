c6ab9fda1e376c5c45e3e162e98272e724dc0e18
XiaoWang <xxw851@case.edu>
2023-10-12 13:50:22 -0400

    Sprint1-account

--- src/main/java/com/gymnomnom/gymnomnom/mapper/AccountMapper.java
+++ src/main/java/com/gymnomnom/gymnomnom/mapper/AccountMapper.java
@@ -2,28 +2,23 @@ package com.gymnomnom.gymnomnom.mapper;
 
 import com.gymnomnom.gymnomnom.pojo.User;
 import org.apache.ibatis.annotations.*;
-import org.springframework.stereotype.Service;
 
 @Mapper
 public interface AccountMapper {
     /**
-     * Create a account and add it to the user_table
-     * @param user
+     * Create an account and add it to the user_table
      */
     @Insert("insert into user_table (name, password, fitness_type, age) VALUE (#{name}, #{password}, #{fitness_type}, #{age})")
     void add(User user);
 
     /**
      * Delete the account base on the id and remove from the user_table
-     * @param id
      */
     @Delete("delete from user_table where id = #{id}")
     void deleteById(Integer id);
 
     /**
      * Search user's name and password
-     * @param user
-     * @return
      */
     @Select("select * from user_table where name = #{name} and password = #{password}")
     User getByNameAndPw(User user);
